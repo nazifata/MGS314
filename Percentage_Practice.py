e394d8d67ceb53c412f1bee4af043e42dfeaa6dd
nazifata <74391543+nazifata@users.noreply.github.com>
2021-09-09 20:50:18 -0400

    Percentage Practice
    File for percentage and variable practice

---
+++ Percentage_Practice.py
@@ -0,0 +1,24 @@
+print(2+6)
+print(6+10*10+6)
+print((6+10)*(10+6))
+print(7+10-(2/2))
+print((100%2)+1)
+print("square of 7 is",7**2)
+a=5
+b=3.2
+c="Hello"
+
+
+print("type of variable a is", type(a))
+print("type of variable b is", type(b))
+print("type of variable c is", type(c))
+
+points_obtained=10
+total_points=100
+
+percentage = (points_obtained / total points) * 100
+print("percentage are",percentage, "%")
+
+
+
+
